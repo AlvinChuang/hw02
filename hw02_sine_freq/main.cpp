39149ece7faf7d25ad8f2dd2cbf7f385cc569597
AlvinChuang <alvin870203@gapp.nthu.edu.tw>
2020-03-29 05:07:52 +0800

    add button, LED, 7-seg display

--- hw02_sine_freq/main.cpp
+++ hw02_sine_freq/main.cpp
@@ -2,46 +2,98 @@
 
 Serial pc( USBTX, USBRX);
 AnalogIn Ain(A0);
+DigitalIn  Switch(SW3);
+DigitalOut redLED(LED1);
+DigitalOut greenLED(LED2);
+BusOut display(D6, D7, D9, D10, D11, D5, D4, D8); // 7_seg display
 
 const int sample = 512; // sampling freq (sampling rate) (>4(2)*freq) (for freq<100Hz)
 float ADCdata[sample]; // index
 int i;
 
-float freq; // signal frequency
+int freq; // signal frequency(round to int)
 int zeroCross; // numbers of zero crossing
 const float offset = 0.2988; // ADC readed input for offset 1V (zero) //doesn't affect freq much
 int firstI; // index of first zero crossing
 int lastI; // index of last zero crossing
 int halfCycle; // number of half cycle (=zeroCross-1)
 
-int main(){
-    while(1){
-        //sample analog input
-        for (i=0; i<sample; i++){ // smaple for 1 sec
-            ADCdata[i] = Ain;
-            wait(1.f/sample); // wait(timeInterval)
-        }
-        // zero crossing detection
-        zeroCross = 0;
-        firstI = -1; // reset firstI
-        lastI = -1; // reset lastI
-        for (i=0; i<sample; i++){
-            if ((ADCdata[i]>offset && ADCdata[i+1]<=offset)
-                || (ADCdata[i]<offset && ADCdata[i+1]>=offset))
-            {
-                if (firstI==-1) firstI=i;
-                lastI = i;
-                zeroCross++;
-            }
-        }
-        halfCycle = zeroCross - 1; // =(number of cycle)*2
-        
-        // freq = numOfCycle / (timeInterval*(lastI-fistI))
-        //freq = (halfCycle/2.f) / ((1.f/sample)*(lastI-firstI+7)); // without correction factor
-        freq = (halfCycle/2.f) / ((1.f/sample)*(lastI-firstI+7)); // +7 is correction factor
-        if (freq<0) freq=0; //avoid negative caused by correction factor
+char table[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F}; // 0~9
+int freqSSD; // =freq
+int freqDigit[4] = {-1, -1, -1, -1}; // digits of freq, LSD~MSD, none is -1
 
-        pc.printf("%f\r\n", freq); // print freq on computer
-        wait(0.5); // wait 0.5 sec for next freq calculation
+int main(){
+    // --------------------------------------------------------------- //
+    // -- Sample the sine wave by AnalogIn Pin A0. ------------------- //
+    // -- Calculate the frequency of the sine wave recieved from A0. - //
+    // -- Send the sine wave data from A0 to PC. --------------------- //
+    // --------------------------------------------------------------- //
+    //sample analog input
+    for (i=0; i<sample; i++){ // smaple for 1 sec
+        ADCdata[i] = Ain;
+        wait(1.f/sample); // wait(timeInterval)
     }
+    // zero crossing detection
+    zeroCross = 0;
+    firstI = -1; // reset firstI
+    lastI = -1; // reset lastI
+    for (i=0; i<sample; i++){
+        if ((ADCdata[i]>offset && ADCdata[i+1]<=offset)
+            || (ADCdata[i]<offset && ADCdata[i+1]>=offset))
+        {
+            if (firstI==-1) firstI=i;
+            lastI = i;
+            zeroCross++;
+        }
+    }
+    halfCycle = zeroCross - 1; // =(number of cycle)*2
+    // freq = numOfCycle / (timeInterval*(lastI-fistI))
+    //freq = (halfCycle/2.f) / ((1.f/sample)*(lastI-firstI+7)); // without correction factor
+    freq = (halfCycle/2.f) / ((1.f/sample)*(lastI-firstI+7)); // +7 is correction factor
+    if (freq<0) freq=0; //avoid negative caused by correction factor
+    // send sine wave data to PC
+    for (i=0; i<sample; i++){
+        pc.printf("%1.3f\r\n", ADCdata[i]); // need time, so can't be in sample loop
+    }
+    // --------------------------------------------------------------- //
+
+
+    // --------------------------------------------------------------- //
+    // -- If button SW3 is pressed, turn on red led, ----------------- //
+    // -- display freq using 7-seg display. -------------------------- //
+    // -- Otherwise, turn off 7-seg display, turn on green led. ------ //
+    // --------------------------------------------------------------- //
+    // get digit of freq form LSB~MSB
+    freqSSD = freq;
+    if (freqSSD==0){
+        freqDigit[0] = 0;
+    }else{
+        for (i=0; freqSSD>0; i++){
+            freqDigit[i] = freqSSD % 10;
+            freqSSD = freqSSD / 10;
+        }
+    }
+    // button, led, display
+    while(1){
+        for (i=3; i>=0; i--){
+            if(Switch == 0){ // 0 is press button
+                greenLED = 1; // 1 is turn off
+                redLED = 0; // 0 is turn on
+                if (freqDigit[i] != -1){
+                    display = (i==0) ? 
+                        (table[freqDigit[i]]+0x80) : table[freqDigit[i]];
+                    wait(1);
+                }
+            }else{ // 1 is not press button
+                redLED = 1;
+                greenLED = 0;
+                display = 0x00;
+                break;
+            }   
+        }
+    }
+    // --------------------------------------------------------------- //
+
+    // ? need faster sampling rate for FFT?
+    // ? adjust wait(1) to wait(0.5) for SSD?
 }
